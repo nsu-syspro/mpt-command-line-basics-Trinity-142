e2970b08511013ea57799a53be8730a18d6fa562
Nikita <n.sarapov.nsk@gmail.com>
2024-09-13 19:36:18 +0700

    task1-2#2

--- solution/task2.sh
+++ solution/task2.sh
@@ -2,5 +2,6 @@ cd data
 mkdir archives
 tar -xf archive-part1.tar -C archives
 unzip archive-part2.zip -d archives
+cd -
 cat $(find -name "foo.txt") $(find -name "bar.txt") $(find -name "baz.txt")
 rm -r archives
